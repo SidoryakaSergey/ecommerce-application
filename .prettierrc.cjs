40e278f924b803e35cf36084faf3ffff2790797c
Thanatus <s.sidoryaka@gmail.com>
2023-08-06 14:03:50 +0300

    chore: add pre-commit script, fix lenline prettier

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
-  printWidth: 120,
+  printWidth: 100,
   bracketSpacing: true,
   arrowParens: 'always',
   bracketSpacing: true,
