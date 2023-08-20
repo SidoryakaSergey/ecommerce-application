6c67b4b0b1ecdb0973ca1b5faf05c7c606550d4e
Thanatus <s.sidoryaka@gmail.com>
2023-08-20 16:56:00 +0300

    feat: add email and password validation

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,48 +1,114 @@
-import React, { useState } from 'react';
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const { control, handleSubmit, formState } = useForm<FormValues>({
+    mode: 'onBlur',
+  });
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // console.log('Email:', email);
-    // console.log('Password:', password);
+  const onSubmit = (data: FormValues) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
   };
+
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="flex flex-col justify-center items-center">
       <h2 className="text-2xl font-semibold mb-4">Welcom</h2>
-      <form className="w-full max-w-sm bg-white p-8 rounded shadow-md" onSubmit={handleSubmit}>
+      <form
+        className="w-full max-w-sm bg-white p-8 rounded shadow-md"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <h2 className="text-2xl font-semibold mb-4">Login</h2>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email*
           </label>
-          <input
-            className="w-full p-2 border border-gray-300 rounded"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+          <Controller
+            name="email"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: 'Email is required',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                message: 'Invalid email address',
+              },
+            }}
+            render={({ field, fieldState }) => (
+              <div>
+                <input
+                  className="w-full p-2 border border-gray-300 rounded"
+                  type="text"
+                  {...field}
+                />
+                {fieldState.error && (
+                  <span className="text-red-500">{fieldState.error.message}</span>
+                )}
+              </div>
+            )}
           />
         </div>
-        <div className="mb-6">
+        {/* ********* */}
+        <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
             Password*
           </label>
-          <input
-            className="w-full p-2 border border-gray-300 rounded"
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+          <Controller
+            name="password"
+            control={control}
+            defaultValue=""
+            rules={{
+              required: 'Password is required',
+              minLength: {
+                value: 8,
+                message: 'Password must be at least 8 characters long',
+              },
+              pattern: {
+                value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]+$/,
+                message: 'Invalid password format',
+              },
+            }}
+            render={({ field, fieldState }) => (
+              <div>
+                <div className="flex items-center">
+                  <input
+                    className="border rounded p-2 mr-2"
+                    type={showPassword ? 'text' : 'password'}
+                    {...field}
+                  />
+                  <button
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="bg-gray-300 p-2 rounded"
+                  >
+                    {showPassword ? (
+                      <EyeSlashIcon className="h-6 w-6" />
+                    ) : (
+                      <EyeIcon className="h-6 w-6" />
+                    )}
+                  </button>
+                </div>
+                {fieldState.error && (
+                  <span className="text-red-500">{fieldState.error.message}</span>
+                )}
+              </div>
+            )}
           />
         </div>
+        {/* ************* */}
         <button
           className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
           type="submit"
+          disabled={formState.isSubmitting}
         >
           Log In
         </button>
