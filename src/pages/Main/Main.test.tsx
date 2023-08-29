0a4339d50f247cdd34af1c7a4f728ae57ef67deb
Alexey Meshin <ou3giu13hg@gmail.com>
2023-08-29 19:54:31 +0300

    feat: add products component and add this to main page

--- src/pages/Main/Main.test.tsx
+++ src/pages/Main/Main.test.tsx
@@ -6,7 +6,7 @@ describe('MainPage', () => {
   it('Renders main content', () => {
     // ARRANGE
     render(<MainPage />);
-    // ACT
+
     // EXPECT
     expect(
       screen.getByRole('heading', {
--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,6 +1,9 @@
+import Product from '../../components/Products/Product.tsx';
+
 function MainPage() {
   return (
     <div className="p-8">
+      <Product />
       <h1>Main</h1>
     </div>
   );
