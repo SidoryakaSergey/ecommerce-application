d46c3ee710a50dd9d4d1272237ee4af8453c2aa2
Thanatus <s.sidoryaka@gmail.com>
2023-09-18 01:10:31 +0300

    refactor: header hover and color link/navlinl

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import UserAccountHeader from './UserAccountHeader';
 
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <header className={styles.headerContainer} style={{ color: '#E5E7EB' }}>
       <div className={styles.logoContainer}>
-        <NavLink to="/">
+        <Link to="/">
           <div className={styles.logoContainer}>
             <div className={`${styles.logoContainer} ${styles.textLogo}`}>
               <h1
@@ -17,11 +17,11 @@ const Header: React.FC = () => {
               </h1>
             </div>
           </div>
-        </NavLink>
+        </Link>
         <div className="h-[36px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100"></div>
-        <NavLink to="/catalog" style={{ fontSize: '20px' }}>
+        <Link to="/catalog" style={{ fontSize: '20px' }}>
           <div>Catalog</div>
-        </NavLink>
+        </Link>
       </div>
       <nav
         style={{
@@ -29,7 +29,7 @@ const Header: React.FC = () => {
           alignItems: 'center',
           fontWeight: 'bold',
           color: '#a94d29ff',
-          gap: '15px',
+          gap: '10px',
         }}
         className={styles.navContainer}
       >
@@ -37,15 +37,6 @@ const Header: React.FC = () => {
           className={styles.navContent}
           style={{ display: 'flex', alignItems: 'center', fontWeight: 'bold', color: '#a94d29ff' }}
         >
-          {/* <nav className={styles.logoContainer}>
-         <NavLink
-          to="/"
-          style={{ color: '#a94d29ff' }}
-          className="text-white font-semibold text-lg hover:opacity-75 transition duration-300"
-        >
-          Main
-        </NavLink>
-        <div className={styles.logoContainer}> */}
           <UserAccountHeader />
         </div>
       </nav>
